e036f3ca8fe085c2a4ca6e39b48eb27c9954df30
ts-pavlo.shmyhol <pashashmigol@gmail.com>
2020-07-30 16:13:30 +0300

    fix crash and bump version to 1.0.4

--- app/src/main/java/com/example/android/radadvertiserdemo/DemoApplication.kt
+++ app/src/main/java/com/example/android/radadvertiserdemo/DemoApplication.kt
@@ -5,8 +5,6 @@ import com.google.firebase.iid.FirebaseInstanceId
 import com.rakuten.attribution.sdk.Configuration
 import com.rakuten.attribution.sdk.RakutenAdvertisingAttribution
 import com.rakutenadvertising.radadvertiserdemo.BuildConfig
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 const val ENDPOINT_URL = "https://attribution-sdk-endpoint-ff5ckcoswq-uc.a.run.app/v2/"
 
@@ -26,8 +24,6 @@ class DemoApplication : Application() {
                 endpointUrl = ENDPOINT_URL,
                 deviceId = FirebaseInstanceId.getInstance().id
         )
-        GlobalScope.launch {
-            RakutenAdvertisingAttribution.setup(applicationContext, configuration)
-        }
+        RakutenAdvertisingAttribution.setup(applicationContext, configuration)
     }
 }
